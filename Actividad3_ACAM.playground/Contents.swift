f034939a375ada50d7d3552ffd60f00cb532ee70
aldocesarmg <aldocesarmg@gmail.com>
2021-02-17 23:37:24 -0600

    Actividad 3

---
+++ Actividad3_ACAM.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+//Tipos de datos
+var dato1:Int = 10
+var dato2:Float = 9.5
+var dato3:String = "Hola"
+var dato4:String = "Mundo"
+
+var dato5 = 20
+var dato6 = 15.6
+var dato7 = "Hello"
+var dato8 = "World"
+
+//Asociación de tipos
+var dato9:String = String()
+var dato10:Int = Int()
+
+//Arreglos y Diccionarios
+var numeros:Array<Int> = Array<Int>()
+numeros.append(1)
+numeros.append(2)
+numeros.append(3)
+numeros.append(4)
+numeros.append(5)
+numeros.append(6)
+numeros.append(7)
+numeros.append(8)
+numeros.append(9)
+numeros.append(10)
+
+var diasSemana:Dictionary<Int, String> = Dictionary<Int, String>()
+diasSemana = [1:"Lunes", 2:"Martes", 3:"Miércoles", 4:"Jueves", 5:"Viernes", 6:"Sábado", 7:"Domingo"]
